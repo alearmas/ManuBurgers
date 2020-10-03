6d2afc0297e5c3ebb7527aa0791d87fb15808106
Alejandro Armas <aarmas17@gmail.com>
2020-10-03 15:33:05 -0300

    Healthy & Deluxe burgers completed

--- src/com/aarmas/Main.java
+++ src/com/aarmas/Main.java
@@ -10,5 +10,10 @@ public class Main {
         burger.addAddition2("Lettuce", 0.75);
         burger.addAddition3("Cheese", 1.13);
         System.out.println("Total burger price is " + burger.totalPrice());
+
+        Healthy healthy = new Healthy("Bacon", 5.67);
+        healthy.addExtra1("Egg", 5.43);
+        healthy.addExtra2("Lentils", 3.41);
+        System.out.println("Total burger price is " + healthy.totalPrice());
     }
 }
